bae4d5e2aaf0445fc0794351e2fe800646b65d59
Abel <abelastra0@gmail.com>
2026-03-23 23:27:47 -0300

    chore: inicialización del proyecto

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from'express'
+import cors from'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+//middlewares 
+app.use(cors())
+app.use(express.json())
+
+// ruta d eprueba 
+
+app.get ('/', (req,res)=>{
+    res.json({mensaje: "servidor funcionando :)"})
+
+})
+app.listen(PORT,()=>{
+    console.log(`SERVIDOR FUNCIONADO EN EL PUERTO ${PORT}`)
+})
