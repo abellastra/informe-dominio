bb45d3452baec35d44b513d5922cd6b457b5abfa
Abel <abelastra0@gmail.com>
2026-03-26 12:22:26 -0300

    feat: endpoints de solicitudes completos

--- backend/src/controllers/solicitudController.js
+++ backend/src/controllers/solicitudController.js
@@ -1,34 +1,74 @@
 import prisma from "../db.js";
 
 export const crearSolicitud = async (req, res) => {
-  try{
-  const { mailCliente, patente, tipoVehiculo } = req.body;
-  if (!mailCliente || !patente || !tipoVehiculo) {
-    return res
-      .status(400)
-      .json({ error: "todos los camopos son obligatorios " });
-  }
+  try {
+    const { mailCliente, patente, tipoVehiculo } = req.body;
+    if (!mailCliente || !patente || !tipoVehiculo) {
+      return res
+        .status(400)
+        .json({ error: "todos los camopos son obligatorios " });
+    }
 
-  const solicitud = await prisma.solicitud.create({
-    data: {
-      mailCliente,
-      patente,
-      tipoVehiculo,
-    },
-  });
-  res.status(201).json(solicitud);
-}catch(error){
-  res.status(500).json({error:"ERROR al crear solicitud"})
-};
- }
+    const solicitud = await prisma.solicitud.create({
+      data: {
+        mailCliente,
+        patente,
+        tipoVehiculo,
+      },
+    });
+    res.status(201).json(solicitud);
+  } catch (error) {
+    res.status(500).json({ error: "ERROR al crear solicitud" })
+  };
+}
 //listar solicitudes 
- export const listarSolicitudes  = async (req, res)=>{
-  try{
-    const solicitudes= await prisma.solicitud.findMany({
-      orderBy:{createdAt:'desc'}
+export const listarSolicitudes = async (req, res) => {
+  try {
+    const solicitudes = await prisma.solicitud.findMany({
+      orderBy: { createdAt: 'desc' }
     })
     res.json(solicitudes)
-  }catch(error){
-    res.status(500).json({error:"error al listar las solicitudes"})
+  } catch (error) {
+    res.status(500).json({ error: "error al listar las solicitudes" })
   }
- }
+}
+
+//obtener solicitud
+
+export const obtenerSolicitudes = async (req,res) => {
+  try {
+    const { id } = req.params
+
+    const solicitud = await prisma.solicitud.findUnique({
+      where: { id: Number(id) }
+    })
+
+    if (!solicitud) {
+      return res.status(404).json({error: 'solicitud no encontrada'})
+    }
+    res.json(solicitud)
+  } catch (error) {
+    res.status(500).json({ error: 'error al obtener las solicitudes ' })
+  }
+}
+
+// actualiza
+
+export const actualizarEstado= async(req,res)=>{
+  try {
+    const{id}=req.params
+    const{estado}=req.body
+
+    const estadoValidos =['pendiente', 'en_proceso','listo']
+    if(!estadoValidos.includes(estado)){
+     return res.status(400).json({error:'estado no valido'})
+    }
+    const solicitud = await prisma.solicitud.update({
+      where:{id:Number(id)},
+      data:{estado}
+    })
+   res.json(solicitud)
+  } catch (error) {
+    res.status(500).json({error:"error al actulizar el estado "})
+  }
+}
--- backend/src/routes/solicitudRoutes.js
+++ backend/src/routes/solicitudRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {
+    actualizarEstado,
   crearSolicitud,
   listarSolicitudes,
+  obtenerSolicitudes,
 } from "../controllers/solicitudController.js";
 
 const router = Router();
@@ -9,5 +11,9 @@ const router = Router();
 router.post("/solicitudes", crearSolicitud);
 
 router.get("/solicitudes", listarSolicitudes);
+
+router.get('/solicitudes/:id',obtenerSolicitudes)
+
+router.patch('/solicitudes/:id', actualizarEstado)
 
 export default router;
